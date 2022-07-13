3bbe120757f90775a575ce15dce5b8d5b29bedd8
Rachana5856 <45552880+Rachana5856@users.noreply.github.com>
2022-07-13 19:56:03 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD_Validation.Startup))]
+namespace CRUD_Validation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
